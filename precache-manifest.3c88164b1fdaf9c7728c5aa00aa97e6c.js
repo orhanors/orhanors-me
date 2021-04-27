f34c5d0b2abf0d9f7bf98dccab758e22fb9dca2e
orhanors <orsorhan1@gmail.com>
2021-04-27 20:12:39 +0300

    Updates

---
+++ precache-manifest.3c88164b1fdaf9c7728c5aa00aa97e6c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80a178987623d84ec88b4c134201856e",
+    "url": "/orhanors-me/index.html"
+  },
+  {
+    "revision": "3aae910a4cad1ed7d795",
+    "url": "/orhanors-me/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "9364820ca08075ea82ff",
+    "url": "/orhanors-me/static/css/main.cc1cfa94.chunk.css"
+  },
+  {
+    "revision": "3aae910a4cad1ed7d795",
+    "url": "/orhanors-me/static/js/2.c646d416.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/orhanors-me/static/js/2.c646d416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9364820ca08075ea82ff",
+    "url": "/orhanors-me/static/js/main.f336acd5.chunk.js"
+  },
+  {
+    "revision": "93bcf3e76fb237d90273",
+    "url": "/orhanors-me/static/js/runtime-main.170ee523.js"
+  },
+  {
+    "revision": "c6bb37146dc5f75a8352640bec12fff6",
+    "url": "/orhanors-me/static/media/myProfile.c6bb3714.jpeg"
+  }
+]);
